dcdc39a8c83b21ddd352256ebdacdcb9ff328041
Tran Xuan Thoi <35894580+xuanthoiqn44@users.noreply.github.com>
2018-05-09 00:10:48 +0700

    done

---
+++ bai2-update/bai2-2/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace bai2_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.panelweb = new System.Windows.Forms.Panel();
+            this.textBoxpass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonsearch = new System.Windows.Forms.Button();
+            this.textBoxmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelcount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Email mới nhất:";
+            // 
+            // panelweb
+            // 
+            this.panelweb.Location = new System.Drawing.Point(15, 96);
+            this.panelweb.Name = "panelweb";
+            this.panelweb.Size = new System.Drawing.Size(773, 344);
+            this.panelweb.TabIndex = 13;
+            // 
+            // textBoxpass
+            // 
+            this.textBoxpass.Location = new System.Drawing.Point(373, 15);
+            this.textBoxpass.Name = "textBoxpass";
+            this.textBoxpass.PasswordChar = '*';
+            this.textBoxpass.Size = new System.Drawing.Size(183, 20);
+            this.textBoxpass.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(311, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Password:";
+            // 
+            // buttonsearch
+            // 
+            this.buttonsearch.Location = new System.Drawing.Point(572, 15);
+            this.buttonsearch.Name = "buttonsearch";
+            this.buttonsearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonsearch.TabIndex = 10;
+            this.buttonsearch.Text = "Search";
+            this.buttonsearch.UseVisualStyleBackColor = true;
+            this.buttonsearch.Click += new System.EventHandler(this.buttonsearch_Click);
+            // 
+            // textBoxmail
+            // 
+            this.textBoxmail.Location = new System.Drawing.Point(47, 11);
+            this.textBoxmail.Name = "textBoxmail";
+            this.textBoxmail.Size = new System.Drawing.Size(230, 20);
+            this.textBoxmail.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Mail:";
+            // 
+            // labelcount
+            // 
+            this.labelcount.AutoSize = true;
+            this.labelcount.Location = new System.Drawing.Point(18, 48);
+            this.labelcount.Name = "labelcount";
+            this.labelcount.Size = new System.Drawing.Size(115, 13);
+            this.labelcount.TabIndex = 15;
+            this.labelcount.Text = "So tin nhan chua doc: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelcount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panelweb);
+            this.Controls.Add(this.textBoxpass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonsearch);
+            this.Controls.Add(this.textBoxmail);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panelweb;
+        private System.Windows.Forms.TextBox textBoxpass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonsearch;
+        private System.Windows.Forms.TextBox textBoxmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelcount;
+    }
+}
+
---
+++ bai2-update/bai2-2/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Xml;
+using System.IO;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+
+namespace bai2_2
+{
+    public partial class Form1 : Form
+    {
+        private class IgnoreBadCert : ICertificatePolicy
+        {
+            public bool CheckValidationResult(ServicePoint sp, X509Certificate cert, WebRequest request, int err)
+            {
+                return true;
+            }
+        }
+        WebBrowser wb;
+        public Form1()
+        {
+            InitializeComponent();
+            wb = new WebBrowser();
+            wb.Width = 800;
+            wb.Height = 600;
+            panelweb.Controls.Add(wb);
+        }
+
+        private void buttonsearch_Click(object sender, EventArgs e)
+        {
+            ServicePointManager.CertificatePolicy = new IgnoreBadCert();
+            NetworkCredential cred = new NetworkCredential();
+            cred.UserName = textBoxmail.Text;
+            cred.Password = textBoxpass.Text;
+            WebRequest webr =
+            WebRequest.Create("https://mail.google.com/mail/feed/atom");
+            webr.Credentials = cred;
+            Stream stream = webr.GetResponse().GetResponseStream();
+            StreamReader sr = new StreamReader(stream);
+            string s = sr.ReadToEnd();
+            s = s.Replace("<feed version=\"0.3\" xmlns=\"http://purl.org/atom/ns#\">", @"<feed>");
+            StreamWriter sw = new StreamWriter("emaildata.txt");
+            sw.Write(s);
+            sr.Close();
+            sw.Close();
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load("emaildata.txt");
+            string nr = xmldoc.SelectSingleNode(@"/feed/fullcount").InnerText;
+            labelcount.Text = "Số tin nhắn chưa đọc: " + nr;
+            //Console.WriteLine("Count: {0}", nr);
+            //string[] names3 = xDoc.XPathSelectElement(@"/feed/entry").ChildrenNames();
+            foreach (XmlNode node in xmldoc.SelectNodes(@"/feed/entry"))
+            {
+                string s1 = node.ChildNodes.Item(2).OuterXml;
+                string regex = "href=\"(.*)\"";
+                Match match = Regex.Match(s1, regex);
+                if (match.Success)
+                {
+                    string link = match.Groups[1].Value;
+                    wb.Navigate(link);
+                }
+                
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
